1351e0f194aab523227ed3f7fa8753606d01bbf0
locT-581 <loct.se581@gmail.com>
2024-11-08 09:21:17 +0700

    add: Button Component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,19 @@
+import { forwardRef } from "react";
+import "./style.scss";
+
+export interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    color: string;
+    text: React.ReactNode;
+    onClick?: () => void;
+}
+
+export default forwardRef<HTMLButtonElement, IButtonProps>(function Button(
+    { color, text, onClick }: IButtonProps,
+    ref
+) {
+    return (
+        <button ref={ref} onClick={onClick} style={{ backgroundColor: color }} className="btn">
+            {text}
+        </button>
+    );
+});
