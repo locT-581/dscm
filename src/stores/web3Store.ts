c5f9d77db65ec36d6ae6bc51f5bc9082f966d4c1
locT-581 <loct.se581@gmail.com>
2024-11-28 12:46:53 +0700

    add: get data from blockchain in store

--- src/stores/web3Store.ts
+++ src/stores/web3Store.ts
@@ -3,8 +3,8 @@ import { Contract } from "web3-eth-contract";
 import { createStore } from "zustand/vanilla";
 
 import { OriginAbi } from "@/types/common";
-import Order from "@/types/order";
-import Shipment from "@/types/shipment";
+import Order, { OrderBlockChainType } from "@/types/order";
+import Shipment, { ShipmentBlockChainType } from "@/types/shipment";
 
 export type StoreState = {
   web3: Web3 | undefined;
@@ -24,6 +24,9 @@ export type StoreAction = {
 
   setOrders: (orders: StoreState["orders"]) => void;
   setShipments: (shipments: StoreState["shipments"]) => void;
+
+  getOrders: () => void;
+  getShipments: () => void;
 };
 
 export type StoreType = StoreState & StoreAction;
@@ -39,7 +42,7 @@ export const defaultInitState: StoreState = {
 };
 
 export const createWeb3Store = (initState: StoreState = defaultInitState) => {
-  return createStore<StoreType>()((set) => ({
+  return createStore<StoreType>()((set, get) => ({
     ...initState,
     setWeb3: (web3: Web3 | undefined) => set(() => ({ web3 })),
     setAccount: (account: string | undefined) => set(() => ({ account })),
@@ -48,5 +51,37 @@ export const createWeb3Store = (initState: StoreState = defaultInitState) => {
 
     setOrders: (orders: Order[] | undefined) => set(() => ({ orders })),
     setShipments: (shipments: Shipment[] | undefined) => set(() => ({ shipments })),
+
+    // fetch Data
+    getOrders: async () => {
+      const { contract } = get();
+      if (!contract) return;
+
+      const orderCount: number = await contract.methods.orderCount().call();
+      const orders: Order[] = [];
+      for (let i = 1; i <= orderCount; i++) {
+        const newOrder: OrderBlockChainType = await contract.methods.orders(i).call();
+        orders.push({ ...newOrder, id: Number(newOrder.id).toString() });
+      }
+      set(() => ({ orders }));
+    },
+
+    getShipments: async () => {
+      const { contract } = get();
+      if (!contract) return;
+
+      // Get shipments from contract
+      const shipmentCount: number = await contract.methods.shipmentCount().call();
+      const shipments: Shipment[] = [];
+      for (let i = 1; i <= shipmentCount; i++) {
+        const newShipment: ShipmentBlockChainType = await contract.methods.shipments(i).call();
+        shipments.push({
+          ...newShipment,
+          id: Number(newShipment.id).toString(),
+          latlong: JSON.parse(newShipment.latlong),
+        });
+      }
+      set(() => ({ shipments }));
+    },
   }));
 };
